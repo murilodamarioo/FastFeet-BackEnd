1980a6d91c2f2fec64b57ce5b8c4367f2b03abfd
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-07 22:34:18 -0300

    :sparkles: feat: Creates Courier repository

---
+++ src/domain/application/repositories/courier-repository.ts
@@ -0,0 +1,21 @@
+import { Courier } from '@domain/enterprise/entities/Courier'
+
+export abstract class CourierRepository {
+
+  /**
+   * Retrieves a courier by its CPF.
+   * 
+   * @param {string} cpf - The CPF of the courier to be retrieved.
+   * @returns {Promise<Courier | null>} A promise that resolves with the found courier, or null if not found.
+   */
+  abstract findByCpf(cpf: string): Promise<Courier | null>
+  
+  /**
+   * Creates a new courier.
+   * 
+   * @param {Courier} courier The courier to be created.
+   * @returns {Promise<void>} A promise that resolves with void after the courier creation.
+   */
+  abstract create(courier: Courier): Promise<void>
+
+}
---
+++ src/test/repositories/in-memory-courier-reposiotry.ts
@@ -0,0 +1,21 @@
+import { CourierRepository } from '@domain/application/repositories/courier-repository'
+import { Courier } from '@domain/enterprise/entities/Courier'
+
+export class InMemoryCourierRepository extends CourierRepository {
+  public couriers: Courier[] = []
+  
+  async findByCpf(cpf: string): Promise<Courier | null> {
+    const courier = this.couriers.find(courier => courier.cpf === cpf)
+
+    if (!courier) {
+      return null
+    }
+
+    return courier
+  }
+
+  async create(courier: Courier): Promise<void> {
+    this.couriers.push(courier)
+  }
+
+}
