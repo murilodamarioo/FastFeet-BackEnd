1ca15f7e07e5f8344150b8da8221ad656038698b
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-07 23:35:39 -0300

    :sparkles: feat: Implements Courier factory

---
+++ src/test/factories/make-courier.ts
@@ -0,0 +1,15 @@
+import { UniqueEntityId } from '@core/entities/unique-entity-id'
+import { Courier, CourierProps } from '@domain/enterprise/entities/Courier'
+import { faker } from '@faker-js/faker/.'
+
+export function makeCourier(override: Partial<CourierProps>, id?: UniqueEntityId) {
+  const courier = Courier.create({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    cpf: faker.string.numeric(11),
+    ...override
+  }, id)
+
+  return courier
+}
