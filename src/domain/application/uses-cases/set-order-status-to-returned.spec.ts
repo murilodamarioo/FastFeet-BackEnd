69868f3dd9d83c4d2b3ce026102434d46371a451
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-20 23:27:30 -0300

    :sparkles: feat: Implements Set order status to returned use case and tests

---
+++ src/domain/application/uses-cases/set-order-status-to-returned.spec.ts
@@ -0,0 +1,55 @@
+import { InMemoryOrderRepository } from '@test/repositories/in-memory-order-repository'
+import { SetOrderStatusToReturnedUseCase } from './set-order-status-to-returned'
+import { InMemoryRecipientRepository } from '@test/repositories/in-memory-recipient-repository'
+import { makeCourier } from '@test/factories/make-courier'
+import { makeOrder } from '@test/factories/make-order'
+import { Status } from '@domain/enterprise/entities/value-object.ts/Status'
+import { SetOrderStatusError } from './errors/set-order-status-error'
+
+let sut: SetOrderStatusToReturnedUseCase
+let inMemoryOrdersRepository: InMemoryOrderRepository
+let inMemoryRecipientRepository: InMemoryRecipientRepository
+
+describe('Set order status to Returned', () => {
+
+  beforeEach(() => {
+    inMemoryRecipientRepository = new InMemoryRecipientRepository()
+    inMemoryOrdersRepository = new InMemoryOrderRepository(inMemoryRecipientRepository)
+    sut = new SetOrderStatusToReturnedUseCase(inMemoryOrdersRepository)
+  })
+
+  it('should be able to change order status to Returned', async () => {
+    const courier = makeCourier()
+
+    const order = makeOrder({
+      courierId: courier.id,
+      status: Status.PICKED_UP
+    })
+    inMemoryOrdersRepository.orders.push(order)
+
+    const response = await sut.execute({
+      orderId: order.id.toString()
+    })
+
+    expect(response.isSuccess()).toBeTruthy()
+    expect(inMemoryOrdersRepository.orders[0].status).toBe(Status.RETURNED)
+  })
+
+  it('should not be able to change order status if the current status is not Picked Up', async () => {
+    const courier = makeCourier()
+
+    const order = makeOrder({
+      courierId: courier.id,
+      status: Status.PENDING
+    })
+    inMemoryOrdersRepository.orders.push(order)
+
+    const response = await sut.execute({
+      orderId: order.id.toString()
+    })
+
+    expect(response.isFailure()).toBeTruthy()
+    expect(response.value).toBeInstanceOf(SetOrderStatusError)
+  })
+
+})
---
+++ src/domain/application/uses-cases/set-order-status-to-returned.ts
@@ -0,0 +1,36 @@
+import { Either, failure, success } from '@core/either'
+import { OrderNotFoundError } from './errors/order-not-found-error'
+import { SetOrderStatusError } from './errors/set-order-status-error'
+import { OrdersRepository } from '../repositories/orders-repository'
+import { Status, StatusUtils } from '@domain/enterprise/entities/value-object.ts/Status'
+
+export interface SetOrderStatusToReturnedRequest {
+  orderId: string
+}
+
+type SetOrderStatusToReturnedResponse = Either<OrderNotFoundError | SetOrderStatusError, null>
+
+export class SetOrderStatusToReturnedUseCase {
+  constructor(private ordersRepository: OrdersRepository) {}
+
+  async execute(
+    { orderId }: SetOrderStatusToReturnedRequest
+  ): Promise<SetOrderStatusToReturnedResponse> {
+
+    const order = await this.ordersRepository.findById(orderId)
+
+    if (!order) {
+      return failure(new OrderNotFoundError())
+    }
+
+    if (!StatusUtils.isPickedUp(order.status)) {
+      return failure(new SetOrderStatusError(order.status, Status.RETURNED))
+    }
+
+    order.status = Status.RETURNED
+
+    await this.ordersRepository.save(order)
+
+    return success(null)
+  }
+}
